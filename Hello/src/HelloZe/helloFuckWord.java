84db0c3d2e8eba06909795b35a94477a6664264d
yesus.santos <yesus.santos@192.168.56.1>
2023-06-01 11:51:46 -0300

    soma de dois numeros

--- Hello/src/HelloZe/helloFuckWord.java
+++ Hello/src/HelloZe/helloFuckWord.java
@@ -2,7 +2,7 @@ package HelloZe;
 
 public class helloFuckWord {
 	public static void main(String[] args) {
-		System.out.println("Hello World professora!!!");
+		System.out.println("Hello World Classe!!!");
 		
 	}
 
